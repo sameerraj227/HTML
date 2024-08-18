e7264e8ee16bb7decdda3b8f95bb39cc710f7b29
sameerraj227 <122544899+sameerraj227@users.noreply.github.com>
2024-08-18 21:24:32 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,18 @@
+// var name= "students name"
+// var nme = "students roll number"
+// console.log(null)
+
+// let department="computer science"
+// let departments="mechanical"
+// console.log(" 🚀 department:",department)
+
+// var year ="2025"
+// let year ="2026"
+// console.log("years:",year)
+    const students = {
+        "name":"sampie",
+        "dob":"10-12-2003",
+        "college":"gla university"
+    };
+    
+    console.log("🚀 students:", students)
